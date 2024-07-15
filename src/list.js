7fbd3b09cec9ae2e268c49454a5d10d101b11ced
Akshara <aksharareddye13@gmail.com>
2024-07-15 18:00:40 +0530

    logincss

--- src/list.js
+++ src/list.js
@@ -4,8 +4,8 @@ import { useNavigate } from "react-router-dom";
 export default function List() {
   const navigate = useNavigate();
   return (
-    <div>
-      <img src="/images/list/list.jpg" />
+    <div >
+      <img src="/images/listl.jpg"  className="w-full"/>
       <button
         onClick={()=>{navigate('/product')}}
         className="absolute top-0 left-0 w-full h-full opacity-0"
